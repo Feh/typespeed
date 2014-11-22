50d6ad412a25893cf63984715162b7c40b721e60
Julius Plenz <julius@plenz.com>
2014-11-22 22:01:02 +0200

    Add a file under /proc to pass information along

--- typespeed.c
+++ typespeed.c
@@ -1,9 +1,32 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+static int typespeed_proc_show(struct seq_file *m, void *v)
+{
+    char string[] = "Can you read me?";
+
+    seq_printf(m, "%s\n", string);
+    return 0;
+}
+
+static int typespeed_proc_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, typespeed_proc_show, NULL);
+}
+
+static const struct file_operations typespeed_proc_fops = {
+    .open       = typespeed_proc_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+};
 
 static int __init typespeed_init(void)
 {
     printk ("Typespeed loaded!\n");
+    proc_create("typespeed", 0, NULL, &typespeed_proc_fops);
     return 0;
 }
 
